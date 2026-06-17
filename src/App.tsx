66c3802a9b7167ab34e4b32f8b4ab417b7d8db2a
Mahmoud Abdelrazek <mahmoudzanitty@gmail.com>
2026-06-17 03:34:51 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react";
+function App(): ReactElement {
+  return (
+    <div className="flex flex-col gap-4">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
